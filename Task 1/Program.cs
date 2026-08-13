848fbf7dc3fce4395ad80ef3d05bc4669c00c936
Adham <adhamsaber2367@gmail.com>
2026-08-13 08:06:03 +0300

    Complete task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DEPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
